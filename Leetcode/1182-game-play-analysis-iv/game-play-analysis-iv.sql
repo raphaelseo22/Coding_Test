924fa746a32e01ac324bf3143e9d0b29e0abbc9d
Raphael Seo <50270615+raphaelseo22@users.noreply.github.com>
2024-04-05 00:59:08 +0900

    Time: 1335 ms (20.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ Leetcode/1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select round(sum(if(datediff(a1.first_login, a2.event_date) = -1, 1, 0)) / count(distinct a1.player_id), 2) as fraction
+from (
+    select player_id, min(event_date) as first_login
+    from Activity
+    group by player_id
+) as a1
+join Activity as a2 on a1.player_id = a2.player_id
