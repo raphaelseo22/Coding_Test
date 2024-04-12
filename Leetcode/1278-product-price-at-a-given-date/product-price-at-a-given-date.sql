f13dddee4a795886289dcc1549a2e1012b8e23a8
Raphael Seo <50270615+raphaelseo22@users.noreply.github.com>
2024-04-12 23:38:53 +0900

    Time: 1402 ms (15.62%) | Memory: 0B (100.00%) - LeetSync

--- Leetcode/1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ Leetcode/1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,13 +1,21 @@
 # Write your MySQL query statement below
-select p1.product_id, new_price as price
-from Products as p1
-left join (
-    select product_id, max(change_date) as change_date
-    from Products as p2
+-- select p1.product_id, new_price as price
+-- from Products as p1
+-- left join (
+--     select product_id, max(change_date) as change_date
+--     from Products as p2
+--     where change_date <= '2019-08-16'
+--     group by product_id
+-- ) as p2 on p1.product_id = p2.product_id and p1.change_date = p2.change_date
+-- where p2.change_date is not null
+select product_id, new_price as price
+from Products
+where (product_id, change_date) in(
+    select distinct product_id, max(change_date) as change_date
+    from Products
     where change_date <= '2019-08-16'
     group by product_id
-) as p2 on p1.product_id = p2.product_id and p1.change_date = p2.change_date
-where p2.change_date is not null
+)
 
 union
 
